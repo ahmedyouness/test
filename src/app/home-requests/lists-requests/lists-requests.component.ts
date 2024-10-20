4ceb1ec4b6623c62db6bf69b065b4db0e2f829b8
ahmedyouness <ahmed.younes.sig@gmail.com>
2024-10-20 22:56:28 +0200

    add delete method

--- src/app/home-requests/lists-requests/lists-requests.component.ts
+++ src/app/home-requests/lists-requests/lists-requests.component.ts
@@ -8,7 +8,7 @@ import { RequestsService } from '../services/data-requests.service';
 })
 export class ListsRequestsComponent {
   requestService = inject(RequestsService);
-  
+
   ngOnInit(): void {
     this.getRequest()
   }
@@ -19,13 +19,13 @@ export class ListsRequestsComponent {
       this.requestService.requestList = JSON.parse(loacalArray)
     }
   }
-  onDelete(emp: any){
-    const index= this.requestService.requestList.findIndex(e => e.id == emp.id)
-    if(confirm('Are you sure??')){
-      this.requestService.requestList.splice(index ,1);
+  onDelete(emp: any) {
+    const index = this.requestService.requestList.findIndex(e => e.id == emp.id)
+    if (confirm('Are you sure??')) {
+      this.requestService.requestList.splice(index, 1);
       localStorage.setItem('angular18local', JSON.stringify(this.requestService.requestList));
       this.getRequest()
     }
-    }
+  }
 
 }
